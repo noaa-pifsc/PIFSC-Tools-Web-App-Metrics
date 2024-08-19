edfa05ba34ba4205098a067c0244f02c146cf5b1
Jesse Abdul <jesse.abdul@noaa.gov>
2024-08-19 13:57:27 -1000

    --Web App Metrics: Version 1.4--
    	Docker Files:
    		python scripts:
    			docker/src/py_scripts/lib/custom_functions.py - updated to allow more flexible matching by specifying a "By" class method.  Updated to save screenshots in the "data/screenshots" folder
    			docker/src/py_scripts/app.py - updated to implement a timer for the entire test suite.  Updated to add more includes and set the download directory for any downloaded files
    
    	Documentation:
    		README.md - was updated to increment the version number
    
    	directory structure:
    		docker/src/data/screenshots/README.txt was added to ensure the data/screenshots directory exists on the docker host machine

--- docker/src/py_scripts/app.py
+++ docker/src/py_scripts/app.py
@@ -1,13 +1,23 @@
 # include required libraries
+import time
+
+#specify the start timer for the entire test suite
+test_suite_start_timer = time.time()
+
+
 import os
 import random
 import string
-import time
+from datetime import datetime, timedelta 
 
 # include selenium libraries
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.select import Select
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
@@ -46,10 +56,14 @@ options = Options()
 options.add_argument('--headless')
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
+options.add_argument("--window-size=1920,1080")
 # options.add_argument('--enable-javascript')
 options.add_argument('ignore-certificate-errors')
 options.set_capability('goog:loggingPrefs', {'performance': 'ALL'})
 
+prefs = {"download.default_directory" : "/app/data"};
+
+options.add_experimental_option("prefs",prefs);
 
 # create the chrome webdriver object
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
@@ -57,16 +71,15 @@ driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), opti
 # enable the network cache
 driver.execute_cdp_cmd("Network.setCacheDisabled", {"cacheDisabled":False})
 
-
 # custom_functions.log_value("load the page", print_log_messages)
 
 # start the timer
 start_timer=round(time.time()*1000)
 
 
-# Open Login Page - Begin:
-
-#request the base_web_url defined in the application configuration
+"""
+START - 1. Request the base_web_url defined in the application configuration
+"""
 
 # check the value of the app_location
 if (project_scenario_config.app_location == "remote"):
@@ -89,7 +102,13 @@ else:
 """
 Custom code for defining web actions and wait conditions goes here including start/stop timers and .csv file content
 """
+custom_functions.log_value("All of the web actions in the test suite have completed", print_log_messages)
 
+# calculate the elapsed time in minutes based on the test_suite_start_timer variable and the current time
+total_time_min=round(((time.time()-test_suite_start_timer)/60), 2)
+
+# log the elapsed time for the entire test suite
+custom_functions.log_value('total elapsed time (min): '+str(total_time_min), print_log_messages)
 
 # selenium driver session cleanup:
 
